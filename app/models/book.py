3b1ce3189d9433451977bd4931f952b90d5b7713
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-31 22:40:30 -0700

    fixed "flask db init" issue

--- app/models/book.py
+++ app/models/book.py
@@ -5,8 +5,8 @@ class Book(db.Model):
     title = db.Column(db.String)
     description = db.Column(db.String)
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
-]
+# books = [
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world.")
+# ]
