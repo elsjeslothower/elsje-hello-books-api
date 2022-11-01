4a49fa2f44f2e17d5f89fe23bcab79bbfa56110a
Elsje Slothower <elsje.slothower@gmail.com>
2022-10-31 22:35:00 -0700

    commented out line 2 on routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 
-from curses import BUTTON2_DOUBLE_CLICKED
+# from curses import BUTTON2_DOUBLE_CLICKED
 from flask import Blueprint, jsonify, abort, make_response
 
 books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
